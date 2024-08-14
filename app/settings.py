1dbe241ee1e27dc17ac86ebb771a950f79a216ee
Michael Martinez <mamartinez@meico.com.co>
2024-08-14 17:02:16 -0500

    Basic authentication

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework",
     "rest_framework.authtoken",
+    "authentication",
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
-urlpatterns = router.urls
-
-urlpatterns += [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls")),
+]
