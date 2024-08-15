197bf7c7893b07ad64d0087e02cd413c86d6d069
Michael Martinez <mamartinez@meico.com.co>
2024-08-15 17:17:39 -0500

    Autenticacion con SimpleJWT

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        print(user.email)
+        print(user.username)
+        token = super().get_token(user)
+
+        token["username"] = user.username
+        token["email"] = user.email
+
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["username", "email"]
--- authentication/views.py
+++ authentication/views.py
@@ -1,44 +1,96 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
-from authentication.serializers import UserSerializer
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from authentication.serializers import UserSerializer, CustomTokenObtainPairSerializer
+from rest_framework import permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 
 @api_view(["POST"])
+@permission_classes([permissions.AllowAny])
 def login(request):
     user = get_object_or_404(User, username=request.data["username"])
+
     if not user.check_password(request.data["password"]):
-        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-    token, _ = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
+        return Response(
+            {"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    token = CustomTokenObtainPairSerializer.get_token(user)
+    user_serializer = UserSerializer(instance=user)
+
+    return Response(
+        {
+            "refresh": str(token),
+            "access": str(token.access_token),
+            "user": user_serializer.data,
+        }
+    )
 
 
 @api_view(["POST"])
+@permission_classes([permissions.AllowAny])
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        print(request.data)
-        serializer.save()
-        user = User.objects.get(username=request.data["username"])
+        user = serializer.save()
         user.set_password(request.data["password"])
         user.save()
-        token = Token.objects.create(user=user)
-        return Response({"token": token.key, "user": serializer.data})
+
+        token = CustomTokenObtainPairSerializer.get_token(user)
+
+        return Response(
+            {
+                "refresh": str(token),
+                "access": str(token.access_token),
+                "user": serializer.data,
+            }
+        )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def test_token(request):
-    return Response("Passed for {}".format(request.user.email))
+    user = request.user
+    token = CustomTokenObtainPairSerializer.get_token(user)
+    user_serializer = UserSerializer(instance=user)
+    return Response(
+        {
+            "refresh": str(token),
+            "access": str(token.access_token),
+            "user": user_serializer.data,
+        }
+    )
+
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def refresh_token(request):
+    refresh_token = request.data.get("refresh")
+
+    if not refresh_token:
+        return Response(
+            {"detail": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    try:
+        token = RefreshToken(refresh_token)
+        user_id = token["user_id"]
+        user = User.objects.get(id=user_id)
+        token = CustomTokenObtainPairSerializer.get_token(user)
+        user_serializer = UserSerializer(instance=user)
+        return Response(
+            {
+                "refresh": str(token),
+                "access": str(token.access_token),
+                "user": user_serializer.data,
+            }
+        )
+    except (InvalidToken, TokenError) as e:
+        return Response({"detail": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+    except User.DoesNotExist:
+        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
